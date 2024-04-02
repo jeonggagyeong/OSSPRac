efed69a6f2445c2c76622fe0017f5deadec9e773
jeonggagyeong <jgg020210@dgu.ac.kr>
2024-04-02 19:29:16 +0900

    ossp1

--- Psub5/main.py
+++ Psub5/main.py
@@ -2,9 +2,11 @@ print("이름을 입력하세요: ",end=' ')
 name = input()
 print("학번을 입력하세요: ",end=' ')
 num = int(input())
+print("학과를 입력하세요: ",end=' ')
 
 print()
 
 print("<출력>")
 print("이름: "+name)
 print("학번: "+str(num))
+print("학과: "+depart)
