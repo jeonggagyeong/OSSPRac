1b21a4ffa2b846115d09b4c1cb4b30881a920b9f
jeonggagyeong <jgg020210@dgu.ac.kr>
2024-04-09 14:12:56 +0900

    0409

--- Psub5/main.py
+++ Psub5/main.py
@@ -4,6 +4,10 @@ print("학번을 입력하세요: ",end=' ')
 num = int(input())
 print("학과를 입력하세요: ",end=' ')
 depart = input()
+print("학교를 입력하세요: ",end=' ')
+school = input()
+
+
 
 
 print()
@@ -11,6 +15,7 @@ print()
 print("<출력>")
 print("이름: "+name)
 print("학번: "+str(num))
-
+print("학과: "+depart)
+print("학교: "+school)
 
 
